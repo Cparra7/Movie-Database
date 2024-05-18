11d5cd181c2d070e9c71a7099c030a77e2108c04
Cristian Parra <108908482+Cparra7@users.noreply.github.com>
2024-05-17 18:26:58 -0700

    Create keyboardInput.java

---
+++ src/Main/keyboardInput.java
@@ -0,0 +1,26 @@
+//NAME: CRISTIAN PARRA 
+
+
+package Main;
+
+import java.util.Scanner;
+
+public class keyboardInput {
+	// Fields
+	private Scanner keyb;
+	
+	// Constructor
+	public keyboardInput(){
+		keyb = new Scanner(System.in);
+	}
+	
+	// Methods
+	public String getKeyboardLine(){
+		return keyb.nextLine();
+	}
+	
+	/* Call this method before you exit the program! Do NOT close the scanner object inside of getKeyboardLine method! */
+	public void closeKeyboard(){
+		keyb.close();
+	}
+}
